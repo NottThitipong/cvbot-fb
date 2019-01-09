c6bf64bdfeefd536a6c3ffb15c31072e0cce73dd
ThitiPong <tr.ku@hotmail.com>
2019-01-09 21:59:31 +0700

    Initial project

---
+++ index.js
@@ -0,0 +1,223 @@
+'use strict';
+// Imports dependencies and set up http server
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      request = require('request');
+
+// creates express http server
+const app = express();
+app.set('port', (process.env.PORT || 8080));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// get token and access FB
+const token = process.env.FB_VERIFY_TOKEN;
+const access = process.env.FB_ACCESS_TOKEN;
+
+// test connect
+app.get('/', (req, res) => {
+    res.send('Hello CVBOT-FB Heroku.');
+});
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let ver_token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+    // Checks if a token and mode is in the query string of the request
+    if (mode && ver_token) {
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === ver_token) {
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);      
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
+    res.status(404).send(' Error 404');
+});
+
+// Creates the endpoint for our webhook
+app.post('/webhook', (req, res) => {
+    let body = req.body;  
+    // Check the webhook event is from a Page subscription
+    if (body.object === 'page') {  
+        // Iterate over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {  
+            // Get the webhook event. entry.messaging is an array, but 
+            // will only ever contain one event, so we get index 0
+            let webhook_event = entry.messaging[0];
+            // Get the sender PSID
+			let sender_psid = webhook_event.sender.id;
+			// handleMessage
+			handleMessage(sender_psid, webhook_event.message);
+            // Check if the event is a message or postback and
+			// pass the event to the appropriate handler function
+			
+            // if (webhook_event.message) {
+            //     handleMessage(sender_psid, webhook_event.message);        
+            // } else if (webhook_event.postback) {
+            //     handlePostback(sender_psid, webhook_event.postback);
+            // }
+      });  
+      // Return a '200 OK' response to all events
+      res.status(200).send('EVENT_RECEIVED');  
+    } else {
+      // Return a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }  
+});
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+    let response;
+    // Check if the message contains text
+    // if (received_message.text) {
+    //   // Create the payload for a basic text message
+    //   response = {
+    //     "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    //   }
+    // } else if (received_message.attachments) {
+    //     // Get the URL of the message attachment
+    //     let attachment_url = received_message.attachments[0].payload.url;
+    //     response = {
+    //       "attachment": {
+    //         "type": "template",
+    //         "payload": {
+    //           "template_type": "generic",
+    //           "elements": [{
+    //             "title": "Is this the right picture?",
+    //             "subtitle": "Tap a button to answer.",
+    //             "image_url": attachment_url,
+    //             "buttons": [
+    //               {
+    //                 "type": "postback",
+    //                 "title": "Yes!",
+    //                 "payload": "yes",
+    //               },
+    //               {
+    //                 "type": "postback",
+    //                 "title": "No!",
+    //                 "payload": "no",
+    //               }
+    //             ],
+    //           }]
+    //         }
+    //       }
+    //     }
+	// }
+	response = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "list",
+				"top_element_style": "compact",
+				"elements": [
+					{
+						"title": "Classic T-Shirt Collection",
+						"subtitle": "See all our colors",
+						"image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
+						"buttons": [
+							{
+								"title": "View",
+								"type": "web_url",
+								"url": "https://peterssendreceiveapp.ngrok.io/collection",
+								"messenger_extensions": true,
+								"webview_height_ratio": "tall",
+								"fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+							}
+						]
+					},
+					{
+						"title": "Classic White T-Shirt",
+						"subtitle": "See all our colors",
+						"default_action": {
+							"type": "web_url",
+							"url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+							"messenger_extensions": false,
+							"webview_height_ratio": "tall"
+						}
+					},
+					{
+						"title": "Classic Blue T-Shirt",
+						"image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+						"subtitle": "100% Cotton, 200% Comfortable",
+						"default_action": {
+							"type": "web_url",
+							"url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+							"messenger_extensions": true,
+							"webview_height_ratio": "tall",
+							"fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+						},
+						"buttons": [
+							{
+								"title": "Shop Now",
+								"type": "web_url",
+								"url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+								"messenger_extensions": true,
+								"webview_height_ratio": "tall",
+								"fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+							}
+						]        
+					}
+				],
+				"buttons": [
+					{
+						"title": "View More",
+						"type": "postback",
+						"payload": "payload"            
+					}
+				]
+			}
+		}
+	}
+    // Sends the response message
+    callSendAPI(sender_psid, response);    
+}
+
+// Handles messaging_postbacks events
+// function handlePostback(sender_psid, received_postback) {
+//     let response;
+//     // Get the payload for the postback
+//     let payload = received_postback.payload;
+//     // Set the response based on the postback payload
+//     if (payload === 'yes') {
+//       response = { "text": "Thanks!" }
+//     } else if (payload === 'no') {
+//       response = { "text": "Oops, try sending another image." }
+//     }
+//     // Send the message to acknowledge the postback
+//     callSendAPI(sender_psid, response);
+// }
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+    // Send the HTTP request to the Messenger Platform
+    request({
+      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "qs": { "access_token": access },
+      "method": "POST",
+      "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    }); 
+}
+
+// Getting Susi up and running.
+app.listen(app.get('port'), function() {
+	console.log('running on port', app.get('port'));
+});
